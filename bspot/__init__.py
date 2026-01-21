6974fd55060cd83c865c5ff66acebd8b4940791d
Kenshi Takayama <kenshi84@gmail.com>
2026-01-21 17:14:20 +0900

    init: read version info from metadata

--- bspot/__init__.py
+++ bspot/__init__.py
@@ -1,2 +1,7 @@
 from pybspot import *
-__version__ = "1.0.0"
+
+from importlib.metadata import PackageNotFoundError, version
+try:
+    __version__ = version("bspot")
+except PackageNotFoundError:
+    __version__ = "0+unknown"
